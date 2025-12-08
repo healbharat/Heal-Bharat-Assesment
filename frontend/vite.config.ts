c44d309ad55b72d5b237cd309f3be53f4c1777a8
Heal Bharat Official <healbharatofficial@gmail.com>
2025-12-08 19:02:16 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,1 +1,35 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+
+    return {
+        base: './',
+
+        plugins: [react()],
+
+        server: {
+            port: 3000,
+            host: '0.0.0.0',
+        },
+
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
+        },
+
+        build: {
+            rollupOptions: {
+                input: path.resolve(__dirname, 'index.html'),
+            }
+        },
+
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        }
+    };
+});
